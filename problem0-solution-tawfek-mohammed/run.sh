50b11c499cc31e43caec55c40ab38dd8d5873d38
tawfek <tawfekaltaae3@gmail.com>
2021-10-21 23:18:15 +0300

    Update run.sh

--- problem0-solution-tawfek-mohammed/run.sh
+++ problem0-solution-tawfek-mohammed/run.sh
@@ -1,7 +1,23 @@
+echo Testing : search an exist item using -i argument  - should return success
 node problem0-solution-tawfek-mohammed.js -i banana
-node problem0-solution-tawfek-mohammed.js --item NOT_IN_FRIDGE
+echo ---------------------------------
+echo Testing : search an exist item using --item argument  - should return success
+node problem0-solution-tawfek-mohammed.js --item apple
+echo ---------------------------------
+echo Testing : search not exist item using --item argument  - should return error
+node problem0-solution-tawfek-mohammed.js --item meat
+echo ---------------------------------
+echo Testing : search a numeric item using --item argument  - should return error
 node problem0-solution-tawfek-mohammed.js --item 33233
-node problem0-solution-tawfek-mohammed.js -f inputs.json -i apple
+echo ---------------------------------
+echo Testing : search an exist item with file name inputs.json using -f argument - should return success
+node problem0-solution-tawfek-mohammed.js -f inputs.json -i onion
+echo ---------------------------------
+echo Testing : search exist item with file name inputs.json  using --file argument  - should return success
 node problem0-solution-tawfek-mohammed.js --file inputs.json -i Melons
+echo ---------------------------------
+echo Testing : search in a file that not exist using -f  - should return error
 node problem0-solution-tawfek-mohammed.js -f not_a_file.json -i banana
+echo ---------------------------------
+echo End of test  , this is not a unit testing 
 $SHELL
