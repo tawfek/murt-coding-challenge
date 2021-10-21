3ee464be1ec1a4aeb872be9537ec19f9e603f4b6
tawfek <tawfekaltaae3@gmail.com>
2021-10-21 22:48:38 +0300

    add Success response

--- problem0-solution-tawfek-mohammed/problem0-solution-tawfek-mohammed.js
+++ problem0-solution-tawfek-mohammed/problem0-solution-tawfek-mohammed.js
@@ -1,120 +1,136 @@
 var fs = require('fs');
 /**
- * How to use : 
- *  options : 
- * [-i] [--item] : the item name to search in the fridge (required)
+ * How to use :
+ *  options :
+ *  [-i] [--item] : the item name to search in the fridge (required)
  * [-f] [--file] : the file name to search in (optional , default inputs.json)
- * the file MUST HAVE at least one array that contains fridge object 
+ * the file MUST HAVE at least one array that contains fridge object
  */
-
 
 /**
  * whereIsMyFood return the item index in the fridge
- * 
+ *
  * @param {Array.<string>} fridge The fridge data that contains the food
  * @param {String} item The name of the food to search for in the fridge
- * 
+ *
  * @returns {integer} Returns the index of item in the fridge
  * @tawfek
  */
-let whereIsMyFood =  (fridge,item)=>{
-  var itemIndex = -1  ;
-  var FridgeCap = fridge.length ;
-  while (FridgeCap--) {
-      if(fridge[FridgeCap]==item){
-          itemIndex = FridgeCap ;
-          break ;
-      } }    
-  return itemIndex ;
+let whereIsMyFood = (fridge, item) => {
+    var itemIndex = -1;
+    var FridgeCap = fridge.length;
+    while (FridgeCap--) {
+        if (fridge[FridgeCap] == item) {
+            itemIndex = FridgeCap;
+            break;
+        }
+    }
+    return itemIndex;
 }
 
-
-
-
-class Exception extends Error{
-  constructor(args,type,val=''){
-    super(args);
-    this.ERROR_TYPE = type
-    if(val!=''){
-    this.RETURNED_VALUE = val
+class ErrorException extends Error {
+    constructor(args, type, val = '',item) {
+        super(args);
+        this.ERROR_TYPE = type
+        this.ITEM_NAME = item 
+        this.RETURNED_VALUE = val
     }
+}
+
+class SuccessException extends Error {
+  constructor(args, item ,i) {
+      super(args);
+      this.STATUS = "SUCCESS"
+      this.ITEM_NAME = item ;
+      this.ITEM_INDEX =  i ;
   }
 }
 
-
-let ReadInputFile = (path='input.json')=>{
-  try {  
-    var data = fs.readFileSync(path, 'utf8');
-    return JSON.parse(data);    
-} catch(e) {
-    return false ;
+let ReadInputFile = (path = 'input.json') => {
+    try {
+        var data = fs.readFileSync(path, 'utf8');
+        return JSON.parse(data);
+    } catch (e) {
+        return false;
+    }
 }
-}
 
-var arguments = process.argv.slice(2); 
-
+var arguments = process
+    .argv
+    .slice(2);
 
 try {
-  
 
+    // Handle file name arguments search for (-f,--file) arg
+    var fileArg = arguments.indexOf('--file') == -1
+        ? arguments.indexOf('-f')
+        : arguments.indexOf('--file');;
 
-// Handle file name arguments search for (-f,--file) arg
-var fileArg = arguments.indexOf('--file')==-1 ? arguments.indexOf('-f') : arguments.indexOf('--file') ; ;
+    // get the file name
+    var fileName = (fileArg !== -1)
+        ? arguments[fileArg + 1]
+        : 'inputs.json';
 
-// get the file name 
-var fileName=(fileArg !== -1)?arguments[fileArg+1]:'inputs.json';
+    // throw error exception when call file argument and not typed the file name
+    if (!fileName) {
+        throw new ErrorException('NO_FILE_SELECTED', 'FILE_ERROR')
+    }
 
-// throw error exception when call file argument and not typed the file name
-if(!fileName){
-  throw new Exception('NO_FILE_SELECTED','FILE_ERROR')
-}
+    // Handle item to search arguments search for (-i,--item) arg
+    var itemArg = arguments.indexOf('--item') == -1
+        ? arguments.indexOf('-i')
+        : arguments.indexOf('--item');
 
+    // get item name to search for
+    var itemNameArg = (itemArg != -1)
+        ? arguments[itemArg + 1]
+        : false;
+    if (!itemNameArg) {
+        throw new ErrorException('NO_ITEM_SELECTED', 'ITEM_ERROR')
+    }
 
-// Handle item to search arguments search for (-i,--item) arg
-var itemArg = arguments.indexOf('--item')==-1 ? arguments.indexOf('-i') : arguments.indexOf('--item') ;
+    // Read file by file name | path
+    let data = ReadInputFile(fileName);
 
-// get item name to search for
-var itemNameArg = (itemArg != -1)?arguments[itemArg+1]: false ;
-  if(!itemNameArg){
-  throw new Exception('NO_ITEM_SELECTED','ITEM_ERROR')
-  }
-  
+    // throw error if file not found or unable to read
+    if (!data) {
+        throw new ErrorException('FILE_NOT_FOUND_OR_INVALID', 'FILE_ERROR');
+    }
 
-  // Read file by file name | path
-  let data = ReadInputFile(fileName) ;
+    // get the first fridge food from data
+    let firstFridge = data[0]['fridge'];
 
-  // throw error if file not found or unable to read 
-  if(!data){throw new Exception('FILE_NOT_FOUND_OR_INVALID','FILE_ERROR');}
+    // find the item in the fridge
+    let findItem = whereIsMyFood(firstFridge, itemNameArg);
 
+    // if item not found throw exception
+    if (findItem == -1) {
+        throw new ErrorException('ITEM_NOT_FOUND_IN_FRIDGE', 'ITEM_ERROR', -1)
+    } else {
 
-  // get the first fridge food from data 
-  let firstFridge = data[0]['fridge'] ;
+        // the item found in the fridge return success message with item name and his
+        // place in the fridge [index]
+        throw new SuccessException('ITEM_FOUND',itemNameArg,findItem)
+        
 
-
-  // find the item in the fridge 
-  let findItem = whereIsMyFood(firstFridge, itemNameArg);
-
-
-  // if item not found throw exception 
-  if(findItem==-1){
-    throw new Exception('ITEM_NOT_FOUND','ITEM_ERROR',-1)
-  }else{
-
-    // the item found in the fridge return sucess message with item name and his place 
-    // in the fridge [index] 
-    let RESPONSE = {'status':'success','item':itemNameArg,'index':findItem} ;
-    console.log(RESPONSE) ;
-
-  }
-  
+    }
 
 } catch (error) {
-    let errorAr = {'status':'error','error_type':error.ERROR_TYPE,'message':error.message,'RETURNED_VALUE':error.RETURNED_VALUE} ;
-    console.log(errorAr) ;
+  let response  ;
+  if(error.STATUS){
+    response = {
+      'status': 'success' ,
+      'item_name':error.ITEM_NAME ,
+      'index' : error.ITEM_INDEX
+    }
+  }else{
+    response = {
+      'status': 'error',
+      'error_type': error.ERROR_TYPE,
+      'message': error.message,
+      'RETURNED_VALUE': error.RETURNED_VALUE
+  };
+  }
+    
+    console.log(response);
 }
-
-
-
-
-
-
--- problem0-solution-tawfek-mohammed/run.sh
+++ problem0-solution-tawfek-mohammed/run.sh
@@ -4,3 +4,4 @@ node problem0-solution-tawfek-mohammed.js --item 33233
 node problem0-solution-tawfek-mohammed.js -f inputs.json -i apple
 node problem0-solution-tawfek-mohammed.js --file inputs.json -i Melons
 node problem0-solution-tawfek-mohammed.js -f not_a_file.json -i banana
+$SHELL
